4cc52782225147a7c299cdc8230d042af69fbd9a
johnlyden <johnlydennc@gmail.com>
2020-03-17 08:18:39 -0500

    testing

--- src/Badge/index.tsx
+++ src/Badge/index.tsx
@@ -1,60 +1,6 @@
 import * as React from 'react';
-// import styled, { css } from 'styled-components';
-// import { background, color, typography } from '../shared/styles';
 
 import * as S from './styles';
-
-// const BadgeWrapper = styled.div`
-//   display: inline-block;
-//   vertical-align: top;
-//   font-size: 11px;
-//   line-height: 12px;
-//   padding: 4px 12px;
-//   border-radius: 3em;
-//   font-weight: ${typography.weight.bold};
-
-//   svg {
-//     height: 12px;
-//     width: 12px;
-//     margin-right: 4px;
-//     margin-top: -2px;
-//   }
-
-//   ${(props: any) =>
-//     props.status === 'positive' &&
-//     css`
-//       color: ${color.positive};
-//       background: ${background.positive};
-//     `};
-
-//   ${(props: any) =>
-//     props.status === 'negative' &&
-//     css`
-//       color: ${color.negative};
-//       background: ${background.negative};
-//     `};
-
-//   ${(props: any) =>
-//     props.status === 'warning' &&
-//     css`
-//       color: ${color.warning};
-//       background: ${background.warning};
-//     `};
-
-//   ${(props: any) =>
-//     props.status === 'error' &&
-//     css`
-//       color: ${color.lightest};
-//       background: ${color.negative};
-//     `};
-
-//   ${(props: any) =>
-//     props.status === 'neutral' &&
-//     css`
-//       color: ${color.dark};
-//       background: ${color.mediumlight};
-//     `};
-// `;
 
 /**
  * **Badges?!** We don't need no stinkin' badges!!
--- src/stories/0-Welcome.stories.js
+++ src/stories/0-Welcome.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { linkTo } from '@storybook/addon-links';
 import { Welcome } from '@storybook/react/demo';
 
@@ -7,7 +8,15 @@ export default {
   component: Welcome,
 };
 
+const Wrap = styled.div`
+  background-color: red;
+  width: 200px;
+  height: 200px;
+`;
+
 export const ToStorybook = () => <Welcome showApp={linkTo('Button')} />;
+
+export const testJohn = () => <Wrap>wrappin</Wrap>;
 
 ToStorybook.story = {
   name: 'to Storybook',
