567dcce3df5dd82756c3cd50e78cc00d9d8c9b7d
johnlyden <johnlydennc@gmail.com>
2020-03-18 10:51:58 -0500

    add Badge props

--- src/Badge/index.tsx
+++ src/Badge/index.tsx
@@ -2,10 +2,14 @@ import * as React from 'react';
 
 import * as S from './styles';
 
+interface Props {
+  status?: string;
+}
 /**
  * **Badges?!** We don't need no stinkin' badges!!
  */
-export function Badge({ ...props }) {
+
+export function Badge({ ...props }: Props) {
   return <S.BadgeWrapper {...props} />;
 }
 
