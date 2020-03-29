2b61b917378978e0edb0b525ade781cf069be05f
johnlyden <johnlydennc@gmail.com>
2020-03-29 10:42:25 -0500

    adding font configs

--- src/shared/global.js
+++ src/shared/global.js
@@ -116,7 +116,6 @@ export const bodyStyles = css`
 `;
 
 export const GlobalStyle = createGlobalStyle`
-  /* @import url('https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,800,900'); */
   @font-face {
     font-family: 'Nexa-Regular';
     src: url('${NexaRegular}') format('opentype');
