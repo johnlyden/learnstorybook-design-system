fd86986e668b5cd9d6d405af505f583bd68c1132
johnlyden <johnlydennc@gmail.com>
2020-03-30 11:10:40 -0500

    add some other pages - organize

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { addDecorator } from "@storybook/react";
-import { withA11y } from "@storybook/addon-a11y";
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
+import { addParameters } from '@storybook/react';
 
-import { GlobalStyle } from "../src/shared/global";
+import { GlobalStyle } from '../src/shared/global';
 
 addDecorator(withA11y);
 addDecorator(story => (
@@ -11,3 +12,14 @@ addDecorator(story => (
     {story()}
   </>
 ));
+
+addParameters({
+  options: {
+    storySort: (a, b) => {
+      console.log(a);
+      return a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true });
+    },
+  },
+});
